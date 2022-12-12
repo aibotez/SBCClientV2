1cf83944203966d061091cfd3d9c4f57ef6069f9
aibotez <2290227486@qq.com>
2022-12-12 20:23:02 +0800

    refl

---
+++ UpdateUi/TransShowUpdate.py
@@ -0,0 +1,9 @@
+import sys,threading
+sys.path.append('..')
+from functools import partial
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class TransShowUpdate():
+    def __init__(self):
+        pass
+    
