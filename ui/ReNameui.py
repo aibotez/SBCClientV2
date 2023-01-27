f23658f267debd6d7350b7d3b6f5b9f8e45fe71a
aibotez <2290227486@qq.com>
2023-01-27 11:48:09 +0800

    refl

--- ui/ReNameui.py
+++ ui/ReNameui.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'ReName.ui'
+# Form implementation generated from reading ui file 'ReName1.ui'
 #
 # Created by: PyQt5 UI code generator 5.10.1
 #
@@ -8,32 +8,23 @@
 
 from PyQt5 import QtCore, QtGui, QtWidgets
 
-class Ui_Form(object):
-    def setupUi(self, Form):
-        Form.setObjectName("Form")
-        Form.resize(434, 353)
-        Form.setLayoutDirection(QtCore.Qt.RightToLeft)
-        Form.setStyleSheet("#Form\n"
+class Ui_Dialog(object):
+    def setupUi(self, Dialog):
+        Dialog.setObjectName("Dialog")
+        Dialog.resize(460, 420)
+        Dialog.setMinimumSize(QtCore.QSize(460, 420))
+        Dialog.setMaximumSize(QtCore.QSize(460, 420))
+        Dialog.setStyleSheet("#Dialog\n"
 "{\n"
-"    background:white;\n"
-"    border:none;\n"
-"}\n"
-"#scrollArea_7\n"
-"{\n"
-"    background:white;\n"
-"    border:none;\n"
-"}\n"
-"\n"
-"#scrollAreaWidgetContents_7 QFrame:hover{\n"
-"    background:#D0D3D4;\n"
-"    border-radius:22px;\n"
-"    opacity:0.5;\n"
-"    }")
-        self.verticalLayout = QtWidgets.QVBoxLayout(Form)
-        self.verticalLayout.setContentsMargins(9, 9, -1, 20)
-        self.verticalLayout.setSpacing(30)
+"    background:#FDFEFE;\n"
+"}")
+        self.verticalLayout = QtWidgets.QVBoxLayout(Dialog)
+        self.verticalLayout.setContentsMargins(0, 0, 0, 10)
+        self.verticalLayout.setSpacing(0)
         self.verticalLayout.setObjectName("verticalLayout")
-        self.frame = QtWidgets.QFrame(Form)
+        spacerItem = QtWidgets.QSpacerItem(20, 38, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
+        self.verticalLayout.addItem(spacerItem)
+        self.frame = QtWidgets.QFrame(Dialog)
         self.frame.setLayoutDirection(QtCore.Qt.RightToLeft)
         self.frame.setFrameShape(QtWidgets.QFrame.StyledPanel)
         self.frame.setFrameShadow(QtWidgets.QFrame.Raised)
@@ -42,8 +33,8 @@ class Ui_Form(object):
         self.horizontalLayout.setContentsMargins(0, 0, 0, 0)
         self.horizontalLayout.setSpacing(0)
         self.horizontalLayout.setObjectName("horizontalLayout")
-        spacerItem = QtWidgets.QSpacerItem(114, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
-        self.horizontalLayout.addItem(spacerItem)
+        spacerItem1 = QtWidgets.QSpacerItem(114, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
+        self.horizontalLayout.addItem(spacerItem1)
         self.label = QtWidgets.QLabel(self.frame)
         sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Preferred)
         sizePolicy.setHorizontalStretch(0)
@@ -56,14 +47,23 @@ class Ui_Form(object):
         self.label.setAutoFillBackground(False)
         self.label.setObjectName("label")
         self.horizontalLayout.addWidget(self.label)
-        spacerItem1 = QtWidgets.QSpacerItem(114, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
-        self.horizontalLayout.addItem(spacerItem1)
+        spacerItem2 = QtWidgets.QSpacerItem(114, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
+        self.horizontalLayout.addItem(spacerItem2)
         self.verticalLayout.addWidget(self.frame)
-        spacerItem2 = QtWidgets.QSpacerItem(20, 0, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
-        self.verticalLayout.addItem(spacerItem2)
-        self.lineEdit = QtWidgets.QLineEdit(Form)
+        spacerItem3 = QtWidgets.QSpacerItem(20, 38, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
+        self.verticalLayout.addItem(spacerItem3)
+        self.frame_2 = QtWidgets.QFrame(Dialog)
+        self.frame_2.setFrameShape(QtWidgets.QFrame.StyledPanel)
+        self.frame_2.setFrameShadow(QtWidgets.QFrame.Raised)
+        self.frame_2.setObjectName("frame_2")
+        self.horizontalLayout_2 = QtWidgets.QHBoxLayout(self.frame_2)
+        self.horizontalLayout_2.setContentsMargins(0, 0, 0, 0)
+        self.horizontalLayout_2.setSpacing(0)
+        self.horizontalLayout_2.setObjectName("horizontalLayout_2")
+        self.lineEdit = QtWidgets.QLineEdit(self.frame_2)
         self.lineEdit.setMinimumSize(QtCore.QSize(400, 30))
         self.lineEdit.setMaximumSize(QtCore.QSize(400, 30))
+        self.lineEdit.setLayoutDirection(QtCore.Qt.RightToLeft)
         self.lineEdit.setStyleSheet("#lineEdit\n"
 "{\n"
 "    background:#EAEDED;\n"
@@ -73,22 +73,41 @@ class Ui_Form(object):
 "}\n"
 "")
         self.lineEdit.setObjectName("lineEdit")
-        self.verticalLayout.addWidget(self.lineEdit)
-        self.pushButton = QtWidgets.QPushButton(Form)
+        self.horizontalLayout_2.addWidget(self.lineEdit)
+        self.verticalLayout.addWidget(self.frame_2)
+        self.frame_3 = QtWidgets.QFrame(Dialog)
+        self.frame_3.setFrameShape(QtWidgets.QFrame.StyledPanel)
+        self.frame_3.setFrameShadow(QtWidgets.QFrame.Raised)
+        self.frame_3.setObjectName("frame_3")
+        self.horizontalLayout_3 = QtWidgets.QHBoxLayout(self.frame_3)
+        self.horizontalLayout_3.setContentsMargins(0, 0, 0, 0)
+        self.horizontalLayout_3.setSpacing(0)
+        self.horizontalLayout_3.setObjectName("horizontalLayout_3")
+        spacerItem4 = QtWidgets.QSpacerItem(33, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
+        self.horizontalLayout_3.addItem(spacerItem4)
+        self.pushButton = QtWidgets.QPushButton(self.frame_3)
         self.pushButton.setMinimumSize(QtCore.QSize(60, 30))
         self.pushButton.setMaximumSize(QtCore.QSize(60, 30))
         self.pushButton.setObjectName("pushButton")
-        self.verticalLayout.addWidget(self.pushButton)
-        self.verticalLayout.setStretch(0, 10)
-        self.verticalLayout.setStretch(2, 2)
+        self.horizontalLayout_3.addWidget(self.pushButton)
+        spacerItem5 = QtWidgets.QSpacerItem(359, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
+        self.horizontalLayout_3.addItem(spacerItem5)
+        self.horizontalLayout_3.setStretch(0, 10)
+        self.horizontalLayout_3.setStretch(1, 1)
+        self.horizontalLayout_3.setStretch(2, 1)
+        self.verticalLayout.addWidget(self.frame_3)
+        self.verticalLayout.setStretch(0, 1)
+        self.verticalLayout.setStretch(1, 5)
+        self.verticalLayout.setStretch(2, 1)
         self.verticalLayout.setStretch(3, 1)
+        self.verticalLayout.setStretch(4, 2)
 
-        self.retranslateUi(Form)
-        QtCore.QMetaObject.connectSlotsByName(Form)
+        self.retranslateUi(Dialog)
+        QtCore.QMetaObject.connectSlotsByName(Dialog)
 
-    def retranslateUi(self, Form):
+    def retranslateUi(self, Dialog):
         _translate = QtCore.QCoreApplication.translate
-        Form.setWindowTitle(_translate("Form", "重命名"))
-        self.label.setText(_translate("Form", "TextLabel"))
-        self.pushButton.setText(_translate("Form", "确定"))
+        Dialog.setWindowTitle(_translate("Dialog", "Dialog"))
+        self.label.setText(_translate("Dialog", "TextLabel"))
+        self.pushButton.setText(_translate("Dialog", "确定"))
 
