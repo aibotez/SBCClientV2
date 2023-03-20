8c98b05e9c8beef3b49144d9612807e4b8ea11fd
aibotez <2290227486@qq.com>
2023-03-20 14:42:53 +0800

    refl.

--- pack/FileOperBDC.py
+++ pack/FileOperBDC.py
@@ -25,9 +25,37 @@ class FileOperClick(QThread):
                 # print('InitWind',Filei)
                 ChosedFiles.append({'size':Filei['size'],'fepath':Filei['fepath'],'fename':Filei['fename'],'isdir':Filei['isdir'],'fepath_base64':Filei['fepath_base64'],'fetype':Filei['fetype']})
         return ChosedFiles
-    def Down(self):
+    def Down(self,share=None):
+        DownInfos = []
         print('downbdc')
         ChosedFiles = self.GetChoseFiles()
         # if ChosedFiles:
         #     self.SignalTranspan.emit()
-        print(ChosedFiles)
+        for i in ChosedFiles:
+            if i['fetype'] != 'folder':
+                DownFaPath = self.ui.DownPath
+                DownInfos.append([i,DownFaPath])
+                # self.Downact(i,DownFaPath)
+            else:
+                # FatherPath0 = self.ui.DownPath+i['fename']+'/'
+                CurPath = i['fepath']
+                queinfo = {}
+                if share:
+                    queinfo = i
+                else:
+                    queinfo = {'path': CurPath}
+                Files = self.ui.SBCRe.GetAllFilesfromFolder(json.dumps(queinfo))
+                if Files['Files']:
+                    for fei in Files['Files']:
+                        if share:
+                            fei['shareinfo'] = i['shareinfo']
+                        # print(fei)
+                        DownFaPath = self.ui.DownPath + i['fename']+'/'+fei['fapath']+'/'
+                        DownFaPath = DownFaPath.replace('//','/')
+                        # DownFaPath = DownFaPath.replace('//','/')
+
+                        # t = threading.Thread(target=self.Downact, args=(fei, DownFaPath,))
+                        # t.setDaemon(True)
+                        # t.start()
+                        DownInfos.append([fei, DownFaPath])
+        print(DownInfos)
